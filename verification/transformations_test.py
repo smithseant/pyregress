2bd6c6687a3a4e37dc108a2378fbac24fc8efad6
Sean <smithseant@gmail.com>
2014-07-25 14:23:13 -0600

    Slight refinement of the testing for gradients of transformations. Test all the transformation types - they all pass. Second derivative still untested.

--- verification/transformations_test.py
+++ verification/transformations_test.py
@@ -8,25 +8,28 @@ from numpy import array, empty, expand_dims, linspace, meshgrid, reshape, vstack
 
 from transforms import Logarithm, Probit, ProbitBeta, Logit
 
-dx = 1e-6
-x1 = linspace(2.0*dx, 1.0-2.0*dx, 20)
-x2 = linspace(2.0*dx, 2.0-2.0*dx, 20)
+dx = 1e-5
+x1 = linspace(1e2*dx, 1.0-1e2*dx, 40)
+x2 = linspace(1e2*dx, 2.0-1e2*dx, 40)
 x1mesh, x2mesh = meshgrid(x1, x2)
 X = vstack((reshape(x1mesh, -1), reshape(x2mesh, -1))).T
 
-def y_func(x):
-    return expand_dims( x.dot(array([0.5, 0.25])), 1)
-Y = y_func(X)
-my_probit = Probit(Y)
-Zprobit = my_probit(Y)
+def fy(x):
+    return expand_dims(x.dot(array([0.5, 0.25])), 1)
+Y = fy(X)
+#trans = Logarithm(Y)
+trans = Probit(Y)
+#trans = ProbitBeta(Y)
+#trans = Logit(Y)
+Zprobit = trans(Y)
 
 grad_Zprobit = empty(X.shape)
 for j in range(X.shape[1]):
     Xminus, Xplus = X.copy(), X.copy()
     Xminus[:, j] -= dx
     Xplus[:, j] += dx
-    grad_Zprobit[:, j] = ((my_probit(y_func(Xplus)) - my_probit(y_func(Xminus))) / (2.0*dx))[:,0]
+    grad_Zprobit[:, j] = (0.5*(trans(fy(Xplus)) - trans(fy(Xminus))) /dx)[:, 0]
 
-Yout, Ygrad_out = my_probit(Zprobit, inverse=True, grad_z=grad_Zprobit)
+Yout, Ygrad_out = trans(Zprobit, inverse=True, grad_z=grad_Zprobit)
 
 print Ygrad_out
