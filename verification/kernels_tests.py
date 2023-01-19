Fixed the corner cases of inferring the gradient while using Xscaling (with or without explicit_basis) and/or transform. Performed some refactoring without affecting behavior: - more descriptive variable names (e.g. Nd -> n_dims), - loosened the restriction of 80 columns to 100, - clarified descriptions in several docstrings.

verification/kernels_tests.py
@@ -9,30 +9,30 @@ from numpy import ndarray, zeros, empty, tile, abs, nanmax
 from numpy.random import randn
 from pyregress import *
 
-Nx = 10
-dx = 1e-4  # This doesn't seem to behave well smaller than 1e-4.
+n_pts = 10
+δx = 1e-4  # This doesn't seem to behave well smaller than 1e-4.
 
 def test_func(X, aniso=None):
-    Nx, Ndim = X.shape[0], X.shape[1]
+    n_pts, n_dims = X.shape[0], X.shape[1]
     if not isinstance(aniso, ndarray):
         f = (X**2).sum(axis=1)
     else:
-        f = zeros(Nx)
-        for i in range(Ndim):
+        f = zeros(n_pts)
+        for i in range(n_dims):
             f += X[:,i]**2 / aniso[i]
     return f
 
 def radius(X, Y, aniso=None):
-    Ndim = X.shape[1]
-    Nx, Ny = X.shape[0], Y.shape[0]
-    Rk = empty((Nx, Ny, Ndim))
-    for k in range(Ndim):
-        Rk[:, :, k] = tile(X[:,[k]], (1, Ny)) - tile(Y[:,[k]].T, (Nx, 1))
+    n_dims = X.shape[1]
+    n_xpts, n_ypts = X.shape[0], Y.shape[0]
+    Rk = empty((n_xpts, n_ypts, n_dims))
+    for k in range(n_dims):
+        Rk[:, :, k] = tile(X[:,[k]], (1, n_ypts)) - tile(Y[:,[k]].T, (n_xpts, 1))
         if isinstance(aniso, ndarray):
             Rk[:, :, k] /= aniso[k]
     return Rk
 
-X = randn(Nx, 2)
+X = randn(n_pts, 2)
 Rk = radius(X, X)
 
 my_kernel = [Noise(w=1.5),
@@ -78,51 +78,48 @@ my_kernel = [Noise(w=1.5),
              RatQuad(w=1.5, l=Jeffreys(2.0), α=Jeffreys(1.5)),
              RatQuad(w=1.5, l=[Jeffreys(2.0), 2.5], α=Jeffreys(1.5)),
              RatQuad(w=1.5, l=[2.5, Jeffreys(2.5)], α=Jeffreys(1.5)),
-             Noise(w=Jeffreys(1.5)) +
-                SquareExp(w=1.5, l=[2.0, Jeffreys(2.5)]),
-             SquareExp(w=1.5, l=Jeffreys(0.8)) *
-                SquareExp(w=1.5, l=Jeffreys(2.5))]
+             Noise(w=Jeffreys(1.5)) + SquareExp(w=1.5, l=[2.0, Jeffreys(2.5)]),
+             SquareExp(w=1.5, l=Jeffreys(0.8)) * SquareExp(w=1.5, l=Jeffreys(2.5))]
 
 my_hyper = my_kernel.get_φ()
 my_gpr = GPI(X, test_func(X), my_kernel, optimize=False)
-my_gpb = GPI(X, test_func(X), my_kernel, explicit_basis=[0, 1],
-             optimize=False)
+my_gpb = GPI(X, test_func(X), my_kernel, explicit_basis=[0, 1], optimize=False)
 
 K, Kp = my_kernel(Rk, grad=True, block_diag=True)
 P, Pp = my_gpr.posterior_φ(my_hyper, grad=True)
 B, Bp = my_gpb.posterior_φ(my_hyper, grad=True)
 
-my_hyper[0] += dx
+my_hyper[0] += δx
 Kplus = my_kernel(Rk, block_diag=True)
 Pplus = my_gpr.posterior_φ(my_hyper, grad=False)
 Bplus = my_gpb.posterior_φ(my_hyper, grad=False)
-my_hyper[0] -= dx
+my_hyper[0] -= δx
 
-my_hyper[0] -= dx
+my_hyper[0] -= δx
 Kminus = my_kernel(Rk, block_diag=True)
 Pminus = my_gpr.posterior_φ(my_hyper, grad=False)
 Bminus = my_gpb.posterior_φ(my_hyper, grad=False)
-my_hyper[0] += dx
+my_hyper[0] += δx
 
-Kd = (Kplus - Kminus) / (2 * dx)
+Kd = (Kplus - Kminus) / (2 * δx)
 print('Kernel Gradient Error (1st dim.):', nanmax(abs(Kd - Kp[:,:,0])/Kd))
-Pd = (Pplus - Pminus) / (2 * dx)
+Pd = (Pplus - Pminus) / (2 * δx)
 print('Posterior Gradient Error (1st dim.):', abs((Pd[0,0] - Pp[0])/Pd[0,0]))
-Bd = (Bplus - Bminus) / (2 * dx)
+Bd = (Bplus - Bminus) / (2 * δx)
 print('Posterior (w/ bases) Gradient Error (1st dim.):', abs((Bd[0,0] - Bp[0])/Bd[0,0]))
 
 if len(my_hyper) > 1:
-    my_hyper[1] += dx
+    my_hyper[1] += δx
     Kplus = my_kernel(Rk, block_diag=True)
     Pplus = my_gpr.hyper_posterior(my_hyper, grad=False)
-    my_hyper[1] -= dx
+    my_hyper[1] -= δx
     
-    my_hyper[1] -= dx
+    my_hyper[1] -= δx
     Kminus = my_kernel(Rk, block_diag=True)
     Pminus = my_gpr.hyper_posterior(my_hyper, grad=False)
-    my_hyper[1] += dx
+    my_hyper[1] += δx
         
-    Kd = (Kplus - Kminus)/(2.0*dx)
+    Kd = (Kplus - Kminus)/(2.0*δx)
     print ('Kernel Gradient Error (2nd dim.):', nanmax(abs(Kd - Kp[:,:,1])/Kd))
-    Pd = (Pplus - Pminus)/(2.0*dx)
+    Pd = (Pplus - Pminus)/(2.0*δx)
     print ('Posterior Gradient Error (2nd dim.):', abs((Pd[0,0] - Pp[1])/Pd[0,0]))

verification/kernels_tests_radial_grad.py
@@ -3,30 +3,30 @@ from numpy.random import randn
 
 from pyregress import *
 
-Nx = 10
-dx = 1e-4  # This doesn't seem to behave well smaller than 1e-4.
+n_pts = 10
+δx = 1e-4  # This doesn't seem to behave well smaller than 1e-4.
 
 def test_func(X, aniso=None):
-    Nx, Ndim = X.shape[0], X.shape[1]
+    n_pts, n_dims = X.shape[0], X.shape[1]
     if not isinstance(aniso, ndarray):
         f = (X**2).sum(axis=1)
     else:
-        f = zeros(Nx)
-        for i in xrange(Ndim):
+        f = zeros(n_pts)
+        for i in range(n_dims):
             f += X[:,i]**2/aniso[i]
     return f
 
 def radius(X, Y, aniso=None):
-    Ndim = X.shape[1]
-    Nx, Ny = X.shape[0], Y.shape[0]
-    Rk = empty((Nx, Ny, Ndim))
-    for k in range(Ndim):
-        Rk[:, :, k] = tile(X[:,[k]], (1, Ny)) - tile(Y[:,[k]].T, (Nx, 1))
+    n_dims = X.shape[1]
+    n_xpts, n_ypts = X.shape[0], Y.shape[0]
+    Rk = empty((n_xpts, n_ypts, n_dims))
+    for k in range(n_dims):
+        Rk[:, :, k] = tile(X[:,[k]], (1, n_ypts)) - tile(Y[:,[k]].T, (n_xpts, 1))
         if isinstance(aniso, ndarray):
             Rk[:, :, k] /= aniso[k]
     return Rk
 
-X = randn(Nx, 1)
+X = randn(n_pts, 1)
 Rk = radius(X, X)
 
 my_kernel = SquareExp(w=Jeffreys(guess=1.5), l=2.0)
@@ -61,22 +61,22 @@ my_kernel = SquareExp(w=Jeffreys(guess=1.5), l=2.0)
 #my_kernel = SquareExp(w=1.5, l=Constant(guess=0.8)) * SquareExp(w=1.5, l=[Constant(guess=2.0), Constant(guess=2.5)])
 
 #my_hyper, hyper_bounds = my_kernel._map_hyper()
-my_gpr = GPP(X, test_func(X), my_kernel)
-my_gpb = GPP(X, test_func(X), my_kernel, explicit_basis=[0])
+my_gpr = GPI(X, test_func(X), my_kernel)
+my_gpb = GPI(X, test_func(X), my_kernel, explicit_basis=[0])
 
 K, Kp, Kpp = my_kernel(Rk, grad_r='Hess')
 
-Rk += dx
+Rk += δx
 Kplus = my_kernel(Rk)
-Rk -= dx
+Rk -= δx
 
-Rk -= dx 
+Rk -= δx 
 Kminus = my_kernel(Rk)
-Rk += dx
+Rk += δx
 
-Kd = (Kplus - Kminus)/(2.0*dx)
+Kd = (Kplus - Kminus)/(2.0*δx)
 print('Kernel Gradient Error (1st dim.):', nanmax(abs(Kd - Kp[:,:,0])/Kd))
-Kdd = (Kplus - 2.0*K + Kminus)/dx**2
+Kdd = (Kplus - 2.0*K + Kminus)/δx**2
 print('Kernel Hessian Error (1st dim.):', nanmax(abs(Kdd - Kpp[:,:,0,0])/Kdd))
-Kdd = (Kplus - 2.0*K + Kminus)/dx**2
+Kdd = (Kplus - 2.0*K + Kminus)/δx**2
 print('Kernel Hessian Error (1st dim.):', nanmax(abs(Kdd - Kpp[:,:,0,0])/Kdd))